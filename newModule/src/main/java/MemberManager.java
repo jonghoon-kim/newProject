edf6efd458688ae9b3a67a010063c692c3f7167b
kim <kshkim624@naver.com>
2020-05-21 17:56:00 +0900

    .

--- newModule/src/main/java/MemberManager.java
+++ newModule/src/main/java/MemberManager.java
@@ -1,9 +1,27 @@
 import dao.base.StringKeyEntityDao;
 
 public class MemberManager {
-        public boolean login(){
+    private String id;
+    private String password;
+    private boolean isLogin;
 
-        }
+    //region singleton
+    private MemberManager() {
+    }
+
+    private static MemberManager _instance;
+
+    public static MemberManager getInstance(){
+        if (_instance == null)
+            _instance = new MemberManager();
+
+        return _instance;
+    }
+
+    //endregion
+    public boolean login(){
+
+     }
 
 
     public void logout() {
